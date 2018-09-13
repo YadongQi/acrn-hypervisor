dc200d552472f35a13f2e6295cac0a6ce4507918
Qi Yadong <yadong.qi@intel.com>
2018-09-13 15:58:45 +0800

    DM: tpm emulator to communicate with swtpm
    This patch will create control channel and command channel
    so as to communicate with swtpm. Based on the 2 channels,
    a set of APIs will be implemented and exposed.
    
    Signed-off-by: Deng Wei <wei.a.deng@intel.com>
    Signed-off-by: Qi Yadong <yadong.qi@intel.com>
    Signed-off-by: yingbinx <yingbinx.zeng@intel.com>
    Reviewed-by: Zhu Bing <bing.zhu@intel.com>

---
+++ devicemodel/hw/platform/tpm/tpm_internal.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2018 Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer
+ * in this position and unchanged.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#ifndef _TPM_INTERNAL_H_
+#define _TPM_INTERNAL_H_
+
+/* TPMCommBuffer will package TPM2 command and
+ * response which are handled by TPM emulator
+ *
+ * locty: the locality TPM emulator used
+ * in & in_len: To indicate the buffer and the
+ *    size for TPM command
+ * out & out_len: To indicate the buffer and
+ *    the size for TPM response
+ */
+typedef struct TPMCommBuffer {
+	uint8_t locty;
+	const uint8_t *in;
+	uint32_t in_len;
+	uint8_t *out;
+	uint32_t out_len;
+	bool selftest_done;
+} TPMCommBuffer;
+
+/* APIs by tpm_emulator.c */
+/* Create Ctrl channel and Cmd channel so as to communicate with SWTPM */
+int init_tpm_emulator(const char *sock_path);
+
+/* Shutdown of SWTPM and close Ctrl channel and Cmd channel */
+void deinit_tpm_emulator(void);
+
+/* Send Ctrl channel command CMD_GET_TPMESTABLISHED to SWTPM */
+bool swtpm_get_tpm_established_flag(void);
+
+/* Send Ctrl channel command CMD_RESET_TPMESTABLISHED to SWTPM */
+int swtpm_reset_tpm_established_flag(void);
+
+/* Send TPM2 command request to SWTPM by using Cmd channel */
+int swtpm_handle_request(TPMCommBuffer *cmd);
+
+/* Initialization for SWTPM */
+int swtpm_startup(size_t buffersize);
+
+/* Cancellation of the current TPM2 command */
+void swtpm_cancel_cmd(void);
+
+#endif
